c2e797cdee7cfb3e59d094da77f245210275c11f
agunrmdniiii <133621622+agunrmdniiii@users.noreply.github.com>
2023-05-15 11:27:15 +0700

    Update simple_queue.py

--- simple_queue.py
+++ simple_queue.py
@@ -1,9 +1,16 @@
-queue = deque([1,2,3,4,5]) print('antrian sekarang: ', queue)
+from collections import deque
 
-queue.append(6) print('antrian masuk: ', 6)
-print('antrian sekarang: ', queue)
+queue = deque([1,2,3,4,5])
+print('antrian sekarang: ',queue)
 
-queue.append(7) print('antrian masuk: ', 7)
-print('antrian sekarang: ', queue)
+queue.append(6)
+print('antrian masuk: ',6)
+print('antrian sekarang: ',queue)
 
-out = queue.popleft() print('antrian keluar: ', out) print('antrian sekarang: ', queue)
+queue.append(7)
+print('antrian masuk: ',7)
+print('antrian sekarang: ',queue)
+
+out = queue.popleft()
+print('antrian keluar: ',out)
+print('antrian sekarang: ',queue)
