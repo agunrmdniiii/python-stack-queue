b403a0b586a559543bd2e06f5456949a28310cda
agunrmdniiii <133621622+agunrmdniiii@users.noreply.github.com>
2023-05-15 10:51:17 +0700

    Update warehouse_stack.py

--- warehouse_stack.py
+++ warehouse_stack.py
@@ -1,7 +1,7 @@
 stack = []
 
 def tambah barang(stack, barang baru): stack.append(barang baru)
-print(f'{barang baru} berhasil ditambahkan ke dalam stack.")
+print("{barang baru} berhasil ditambahkan ke dalam stack.")
 
 def hapus barang terakhir(stack): if1en(stack) == 0:
 print("Stack kosong, tidak ada barang yang dapat dihapus.") else:
@@ -11,4 +11,4 @@ print(I' (barang terakhir) berhasil dihapus dari stack.")
 def tampilkan barang teratas(stack): if len(stack) == 0:
 print("Stack kosong, tidak ada barang yang dapat ditampilkan.") else:
 barang teratas = stack[-1]
-print(f'Barang teratas di dalam stack adalah (barang teratas) .")
+print("Barang teratas di dalam stack adalah (barang teratas) .")
